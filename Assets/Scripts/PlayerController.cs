6969a4e3d583459a73bf97ae81a6d6324a64ed5e
frederik desmet <info@neo-level.com>
2021-01-16 17:51:29 +0100

    Get a reference to the focal point, and make the player move in the direction of it.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,16 @@ public class PlayerController : MonoBehaviour
 
     // Get the Rigidbody class.
     private Rigidbody _playerRifRigidbody;
+    private GameObject focalPoint;
     
     // Start is called before the first frame update
     private void Start()
     {
         // Get the rigidbody component from the object.
         _playerRifRigidbody = GetComponent<Rigidbody>();
+        
+        // get the focal point game object in our scene.
+        focalPoint = GameObject.Find("FocalPoint");
         
     }
 
@@ -24,7 +28,7 @@ public class PlayerController : MonoBehaviour
         // Get the "Vertical input" from the arrow keys that the player uses.
         float forwardInput = Input.GetAxis("Vertical");
         
-        // Add force when the player uses his input.
-        _playerRifRigidbody.AddForce(Vector3.forward * (speed * forwardInput));
+        // Add force when the player uses his input. with the focal point as reference.
+        _playerRifRigidbody.AddForce(focalPoint.transform.forward * (speed * forwardInput));
     }
 }
