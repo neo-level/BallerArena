5fbf8a3654ef14f2f9224ef627d59181d1280bfa
frederik desmet <info@neo-level.com>
2021-01-16 17:35:23 +0100

    Enable the player to rotate the camera.

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    // Placeholder for the rotation speed, can be changed through in the component.
+    public float rotationSpeed;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Get the horizontal input from the user.
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Rotate the camera around the focal point when the user uses the axis.
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
         
     }
 }
