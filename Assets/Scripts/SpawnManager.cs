b692d15277b34d920ebd80f0ba2a10bd3818ce4c
frederik desmet <info@neo-level.com>
2021-01-16 18:48:46 +0100

    Randomly generate the spawn position.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,29 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
 
+    // Set the range where the enemies can spawn.
+    private float _spawnRange = 9.0f;
+
     // Start is called before the first frame update
     private void Start()
     {
-        // Instantiate a new enemy prefab, spawning the enemy at the given location.
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        SpawnEnemyOnRandomLocation();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+    }
+
+    private void SpawnEnemyOnRandomLocation()
+    {
+        float spawnPositionX = Random.Range(-_spawnRange, _spawnRange);
+        float spawnPositionY = Random.Range(-_spawnRange, _spawnRange);
+
+        // Create a vector with random generated numbers.
+        Vector3 randomPosition = new Vector3(x: spawnPositionX, y: spawnPositionY, z: 0);
+
+        // Instantiate a new enemy prefab, spawning the enemy at the given location.
+        Instantiate(enemyPrefab, position: randomPosition, rotation: enemyPrefab.transform.rotation);
     }
 }
