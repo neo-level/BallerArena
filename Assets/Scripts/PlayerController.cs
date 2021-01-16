7e26e9b6d73f2b95f449500681847ac89746403d
frederik desmet <info@neo-level.com>
2021-01-16 20:40:56 +0100

    Destroy the power up when the player collides with it, added a boolean to keep track when the player got the boost with the trigger and tag created before.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,16 +11,19 @@ public class PlayerController : MonoBehaviour
     // Get the Rigidbody class.
     private Rigidbody _playerRifRigidbody;
     private GameObject _focalPoint;
-    
+
+    // Set the boolean to validate the power up has been taken.
+    public bool hasPowerUp;
+
+
     // Start is called before the first frame update
     private void Start()
     {
         // Get the rigidbody component from the object.
         _playerRifRigidbody = GetComponent<Rigidbody>();
-        
+
         // get the focal point game object in our scene.
         _focalPoint = GameObject.Find("FocalPoint");
-        
     }
 
     // Update is called once per frame
@@ -27,8 +31,18 @@ public class PlayerController : MonoBehaviour
     {
         // Get the "Vertical input" from the arrow keys that the player uses.
         float forwardInput = Input.GetAxis("Vertical");
-        
+
         // Add force when the player uses his input. with the focal point as reference.
         _playerRifRigidbody.AddForce(_focalPoint.transform.forward * (_speed * forwardInput));
+    }
+
+    // Checks if the player collides with another object, perform action accordingly.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
     }
 }
